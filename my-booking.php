31e044fc7a8e3b2af1c4294314b9e8ee563868ba
Alexander <sanya.diplom@mail.ru>
2025-03-09 15:54:23 +0300

    Update my-booking.php
    Вывод брони в личный кабинет

--- my-booking.php
+++ my-booking.php
@@ -3,6 +3,8 @@
         include('vendor/components/account-header.php');
 ?>
 <main class="main">
+    <div class="container">
+        <div class="cards_row">
     <?php
     // Проверяем на права доступа
     if(!isset($_SESSION['users'])){
@@ -17,10 +19,35 @@
     else{
         while($row=$result->fetch_assoc())
         {
-            
+            $app_id=$row['apparts_id'];
+            $result = $connect->query("SELECT *, (SELECT `name` FROM `projects` WHERE `id`=`project_id`) as `project`,
+    (SELECT `name` FROM `type_apparts` WHERE `id`=`type_id`) as `type`,
+    (SELECT `name` FROM `booking_status` WHERE `id`=`status_id`) as `status` FROM `apparts` WHERE `id`=$app_id");
+    if(!$result){
+    return die ("Ошибка получения данных: ". $connect->error);
+    }?>
+                <?php while($row = $result->fetch_assoc()){
+                    ?>
+                    <div class = 'card'>
+                    <h1 class = 'card_title'><?php echo $row['type'] . " ". $row['square_1']. "кв. м ". $row['price']. "₽" ?></h1>
+                    <a href="<?php echo $row['img']?>" target="_blank"><img class = 'card_img' src="<?php echo $row['img'] ?>"></a>
+                        <div class = 'card_body'>
+                            <h3 class = 'card_title'><?php echo "Проект". " ". $row['project'] ?></h3>
+                            <div class="card_text">Корпус: <?php echo $row['corpus']?> 
+                            Секция: <?php echo $row['section']?> 
+                            Номер квартиры:<?php echo $row['number']?> 
+                            Этаж:<?php echo $row['floor']?></div>
+                            <div class="card_text">Жилая площадь:<?php echo $row['square_2']."кв. м"?></div>
+                            <div class="card_text">Высота потолка:<?php echo $row['ceiling_height']."м"?></div>
+                            <div class="card_text">Отделка:<?php echo $row['finishing']?></div>
+                            <div class="card_text">Статус бронирования:<?php echo $row['status']?></div>
+                    <?php    } ?>
+    <?php
         }
     }
     ?>
+    </div>
+</div>
 </main>
 <!--Добавляем подвал-->
 <?php
